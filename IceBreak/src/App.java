3b5e67c0a9a67b0d9b4efddd66105df3484ed33a
AflahPS <aflahps7@gmail.com>
2022-07-12 21:34:58 +0530

    Fedora to JavaReadyFedora

--- IceBreak/src/App.java
+++ IceBreak/src/App.java
@@ -7,18 +7,21 @@ public class App {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int array[];
+        
 
         System.out.println("PLease enter the array size: ");
         int arraySize= input.nextInt();
-        for(int i=0;i<arraySize;++I){
+
+        int array[]= new int[arraySize];
+
+        for(int i=0;i<arraySize;i++){
             System.out.println("number-"+i+1+": ");
             array[i]=input.nextInt();
         }
 
         System.out.println("Array is :");
-        for(int i=0;i<arraySize;++I){
-            System.out.println("number-"+i+1+": "+array[i]);
+        for(int i=0;i<arraySize;++i){
+            System.out.println("number-"+(i+1)+": "+array[i]);
             
         }
     
